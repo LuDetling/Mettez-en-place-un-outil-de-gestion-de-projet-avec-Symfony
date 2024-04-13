ce3b2d399513ea4ecf8cff2eddcbde4f626e00ad
Lucas <lucas.detling@gmail.com>
2024-04-13 13:06:41 +0200

    start create

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -3,23 +3,30 @@
 namespace App\Controller;
 
 use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+// use App\Entity\Car;
 
 class CarController extends AbstractController
 {
-    #[Route('/addcar', name: 'addcar')]
-    public function addCar(Request $request): Response
+    #[Route('/addcar', name: 'addcar', methods: ['GET', 'POST'])]
+    public function addCar(Request $request, CarRepository $carRepository): Response
     {
+        // $car = new Car();
+        // $car->setName();
+        // $form = $this->createForm(CarType::class, $car);
+
         return $this->render('car/addcar.html.twig');
     }
 
     #[Route('/showcar/{id}', name: 'showcar')]
-    public function showCar(Request $request, CarRepository $repository, int $id): Response
+    public function showCar(Request $request, CarRepository $carRepository, int $id): Response
     {
-        $car = $repository->find($id);
+        $car = $carRepository->find($id);
+
         return $this->render('car/showcar.html.twig', [
             'car' => $car
         ]);
